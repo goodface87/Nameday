bc8350fd8ec25f45dad9f1d94ac9fb4fe7e77b66
goodf <goodf@DESKTOP-OCJ7R3O>
2016-05-02 13:34:44 -0700

    Added filtering.

--- Nameday/MainPageData.cs
+++ Nameday/MainPageData.cs
@@ -1,25 +1,106 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Nameday
 {
-    public class MainPageData
+    public class MainPageData : INotifyPropertyChanged
     {
-        public string Greeting { get; set; } = "Hello world";
-        public List<NamedayModel> Namedays { get; set; }
+        private List<NamedayModel> _allNamedays = new List<NamedayModel>();
+
+        private string _greeting = "Hello world.";
+
+        public string Greeting
+        {
+            get { return _greeting; }
+            set {
+                if (value == _greeting)
+                    return;
+                _greeting = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Greeting)));
+            }
+        }
+
+
+        public ObservableCollection<NamedayModel> Namedays { get; set; }
 
         public MainPageData()
         {
-            Namedays = new List<NamedayModel>();
+            Namedays = new ObservableCollection<NamedayModel>();
             for(int month = 1; month <= 12; month++)
             {
-                Namedays.Add(new NamedayModel(month, 1, new string[] { "Adam" }));
-                Namedays.Add(new NamedayModel(month, 1, new string[] { "Eve", "Andrew" }));
+                _allNamedays.Add(new NamedayModel(month, 1, new string[] { "Adam" }));
+                _allNamedays.Add(new NamedayModel(month, 24, new string[] { "Eve", "Andrew" }));
 
             }
+
+            PerformFiltering();
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private NamedayModel _selectedNameday;
+        
+        public NamedayModel SelectedNameday
+        {
+            get { return _selectedNameday; }
+            set
+            {
+                if (value == null)
+                    Greeting = "Hello World!";
+                else
+                    Greeting = "Hello " + value.NameAsString;
+                     
+                _selectedNameday = value;
+            }
+        }
+
+        private string _filter;
+
+        public string Filter
+        {
+            get { return _filter; }
+            set
+            {
+                if (value == _filter)
+                    return;
+                _filter = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Filter)));
+                PerformFiltering();
+            }
+        }
+
+        private void PerformFiltering()
+        {
+            if (_filter == null)
+                _filter = "";
+
+            var lowerCaseFilter = Filter.ToLowerInvariant().Trim();
+
+            var result = _allNamedays.Where(d => d.NameAsString.ToLowerInvariant().Contains(lowerCaseFilter)).ToList();
+
+            var toRemove = Namedays.Except(result).ToList();
+
+            foreach (var x in toRemove)
+                Namedays.Remove(x);
+
+            var resultCount = result.Count;
+            for(int i = 0; i < resultCount; i++)
+            {
+                var resultItem = result[i];
+                if (i + 1 > Namedays.Count || !Namedays[i].Equals(resultItem))
+                    Namedays.Insert(i, resultItem);
+            }
+
+        }
+
+
+
+
+
     }
 }
