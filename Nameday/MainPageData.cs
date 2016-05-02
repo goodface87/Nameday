803036b4d45256c540a7021ca4c598e7a1381d70
goodf <goodf@DESKTOP-OCJ7R3O>
2016-05-02 12:42:15 -0700

    Added NamedayModel and fake data.

--- Nameday/MainPageData.cs
+++ Nameday/MainPageData.cs
@@ -9,5 +9,17 @@ namespace Nameday
     public class MainPageData
     {
         public string Greeting { get; set; } = "Hello world";
+        public List<NamedayModel> Namedays { get; set; }
+
+        public MainPageData()
+        {
+            Namedays = new List<NamedayModel>();
+            for(int month = 1; month <= 12; month++)
+            {
+                Namedays.Add(new NamedayModel(month, 1, new string[] { "Adam" }));
+                Namedays.Add(new NamedayModel(month, 1, new string[] { "Eve", "Andrew" }));
+
+            }
+        }
     }
 }
---
+++ Nameday/NamedayModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nameday
+{
+    public class NamedayModel
+    {
+        public int Month { get; set; }
+        public int Day { get; set; }
+        public IEnumerable<string> Names { get; set; }
+
+        public NamedayModel(int month, int day, IEnumerable<string> names)
+        {
+            Month = month;
+            Day = day;
+            Names = names;
+        }
+
+        public string NameAsString => string.Join(", ", Names);
+
+        //public string NameAsString
+        //{
+        //    get { return string.Join(", ", Names); }
+        //}
+    }
+}
